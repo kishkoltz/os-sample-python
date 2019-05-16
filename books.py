aed323f3190265e7ff4e7d2491d8aa34baac898f
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2019-05-16 21:10:37 +0200

    Update books.py

--- books.py
+++ books.py
@@ -1,7 +1,7 @@
-from app import app, db
+from wsgi import application, db
 from app.models import Author, Book
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Author': Author, "Book":Book}
 
