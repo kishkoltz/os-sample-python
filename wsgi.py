9ada97babed0c299bf87922c17a50955d7d94ab2
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2019-05-16 18:58:30 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
-from flask import Flask
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "Hello cruel world."
+from app import app as application
 
 if __name__ == "__main__":
     application.run()
