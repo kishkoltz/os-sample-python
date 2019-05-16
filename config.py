adec2d5d4b0739cbeb6647b957ecdb948636397b
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2019-05-16 19:28:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,12 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+import os
+basedir = os.environ.get('OPENSHIFT_DATA_DIR')
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'potatoes'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATION = False
